d09f5637198c12f8d6ec4749cee8a154f87fd745
Vladosbandosss <dasistperfektos@gmail.com>
2021-11-09 09:36:59 +0300

    RG
    RG

---
+++ Scripts/LevelSpawnew.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSpawnew : MonoBehaviour
+{
+    public GameObject[] levels;
+
+    private float levelDistance = 50f;//каждый лвл стоко)
+    private float currentLevelX = 0f;
+
+    private float incrementSpawnerX = 125f;
+
+    private int spawnCount = 5;
+
+    void Start()
+    {
+        InvokeRepeating(nameof(SpawnLevels),0f,10f);
+    }
+
+   void SpawnLevels()
+    {
+        int rand = 0;
+
+        for (int i = 0; i < spawnCount; i++)
+        {
+            rand = Random.Range(0, levels.Length);
+
+            GameObject go = Instantiate(levels[rand]);
+
+            Vector3 levelTemp = Vector3.zero;
+            levelTemp.x = currentLevelX;
+
+            go.transform.position = levelTemp;
+
+            currentLevelX += levelDistance;
+            
+        }
+    }
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private AudioSource audioFX;
+
+    public float forwardSpeed = 8f;
+    public float moveSpeed = 8f;
+    public float jumpForce = 300f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private float movementZ;
+
+    private bool canMove;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        audioFX = GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        Invoke(nameof(ActivateMoveMent), 2f);
+    }
+
+    private void FixedUpdate()
+    {
+        CharacterMove();
+        CharacterJump();
+    }
+
+    void CharacterMove()
+    {
+        if (!canMove)
+        {
+            return;
+        }
+
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            movementZ = -moveSpeed;//zэто будет влево и вправо перса повернули из за сцен
+
+        }else if(Input.GetAxisRaw("Horizontal") < 0)
+        {
+            movementZ = moveSpeed;
+        }
+        else
+        {
+            movementZ = 0;
+        }
+
+        moveDirection = new Vector3(forwardSpeed, rb.velocity.y, movementZ);//прямо пойдет сам!лево право мы ,z-потому что мы его повернули из за сцен
+        rb.velocity = moveDirection;
+    }
+
+    void CharacterJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (rb.velocity.y == 0f)
+            {
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            }
+        }
+    }
+
+    void ActivateMoveMent()
+    {
+        canMove = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "KillZone") {
+            if (!canMove)
+            {
+                return;
+            }
+
+            rb.velocity = Vector3.zero;
+
+            canMove = false;
+            audioFX.Play();
+
+            Invoke(nameof(RestartGame), 2f);
+        }
+    }
+
+    private void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
